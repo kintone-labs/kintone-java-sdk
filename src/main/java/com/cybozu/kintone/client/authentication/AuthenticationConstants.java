a3f2b5dc731b8800e023e3f51f16caaf650e0feb
okahashi117 <naruki_okahashi@jbat.co.jp>
2019-02-19 17:11:37 +0900

    Replace tab to blanks and remove a redundat blank line.

--- src/main/java/com/cybozu/kintone/client/authentication/AuthenticationConstants.java
+++ src/main/java/com/cybozu/kintone/client/authentication/AuthenticationConstants.java
@@ -8,9 +8,8 @@
 package com.cybozu.kintone.client.authentication;
 
 public class AuthenticationConstants {
-	public static final String HEADER_KEY_AUTH_PASSWORD = "X-Cybozu-Authorization";
+    public static final String HEADER_KEY_AUTH_PASSWORD = "X-Cybozu-Authorization";
     public static final String HEADER_KEY_AUTH_APITOKEN = "X-Cybozu-API-Token";
     public static final String HEADER_KEY_AUTH_BASIC = "Authorization";
     public static final String AUTH_BASIC_PREFIX = "Basic ";
-
 }
