fbcff3cb28122314bea3a964e7c20dde3958ae78
Ho Kim Cuc <c001106@PC-001106-4.local>
2019-06-25 17:12:06 +0700

    creare record cursor module

---
+++ src/main/java/com/cybozu/kintone/client/module/recordCursor/RecordCursor.java
@@ -0,0 +1,73 @@
+package com.cybozu.kintone.client.module.recordCursor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import com.cybozu.kintone.client.connection.Connection;
+import com.cybozu.kintone.client.connection.ConnectionConstants;
+import com.cybozu.kintone.client.exception.KintoneAPIException;
+import com.cybozu.kintone.client.model.cursor.CreateRecordCursorRequest;
+import com.cybozu.kintone.client.model.cursor.CreateRecordCursorResponse;
+import com.cybozu.kintone.client.model.cursor.GetRecordCursorRequest;
+import com.cybozu.kintone.client.model.cursor.GetRecordCursorResponse;
+import com.cybozu.kintone.client.model.record.GetRecordsResponse;
+import com.cybozu.kintone.client.model.record.field.FieldValue;
+import com.cybozu.kintone.client.module.parser.CursorParser;
+import com.cybozu.kintone.client.module.parser.Parser;
+import com.google.gson.JsonElement;
+
+public class RecordCursor extends Parser{
+	private static final CursorParser parser = new CursorParser();
+    private Connection connection;
+    
+	public RecordCursor(Connection connection) {
+        this.connection = connection;
+    }
+	
+	public CreateRecordCursorResponse createCursor(Integer app, ArrayList<String> fields, String query, Integer size) throws KintoneAPIException {
+		CreateRecordCursorRequest createRecordCursorRequest = new CreateRecordCursorRequest(app, fields, query, size);
+		String requestBody = (String) super.parseObject(createRecordCursorRequest);
+		
+		JsonElement response = this.connection.request(ConnectionConstants.POST_REQUEST, ConnectionConstants.RECORDS_CURSOR,
+                requestBody);
+		return (CreateRecordCursorResponse) super.parseJson(response, CreateRecordCursorResponse.class);
+	}
+	
+	public GetRecordCursorResponse getRecords(String cursorID) throws KintoneAPIException {
+		GetRecordCursorRequest recordCursorRequest = new GetRecordCursorRequest(cursorID);
+		String requestBody = (String) super.parseObject(recordCursorRequest);
+		JsonElement response = this.connection.request(ConnectionConstants.GET_REQUEST, ConnectionConstants.RECORDS_CURSOR,
+                requestBody);
+		return parser.parseForGetRecordCursorResponse(response);
+	}
+	
+	public GetRecordsResponse getAllRecords(String cursorID) throws KintoneAPIException {
+		GetRecordCursorRequest recordCursorRequest = new GetRecordCursorRequest(cursorID);
+		String requestBody = (String) super.parseObject(recordCursorRequest);
+		GetRecordCursorResponse getRecordCursorResponse = new GetRecordCursorResponse();
+		getRecordCursorResponse.setNext(true);
+		
+		ArrayList<HashMap<String, FieldValue>> records = new ArrayList<HashMap<String, FieldValue>>();
+		while (getRecordCursorResponse.getNext() == true) {
+			JsonElement response = this.connection.request(ConnectionConstants.GET_REQUEST, ConnectionConstants.RECORDS_CURSOR,
+	                requestBody);
+			getRecordCursorResponse = parser.parseForGetRecordCursorResponse(response);
+			records.addAll(getRecordCursorResponse.getRecords());
+		}
+		GetRecordsResponse getRecordsResponse = new GetRecordsResponse();
+		getRecordsResponse.setRecords(records);
+		getRecordsResponse.setTotalCount(records.size());
+		
+		return getRecordsResponse;
+	}
+	
+	public void deleteCursor(String cursorID) throws KintoneAPIException {
+		GetRecordCursorRequest recordCursorRequest = new GetRecordCursorRequest(cursorID);
+		String requestBody = (String) super.parseObject(recordCursorRequest);
+		
+		this.connection.request(ConnectionConstants.DELETE_REQUEST, ConnectionConstants.RECORDS_CURSOR,
+                requestBody);
+	}
+}
