41c1461c5a0b0b3ee81693bb60e9d2de6d284447
Nhat Nguyen <phongnhat19@gmail.com>
2019-07-03 10:14:34 +0700

    commit Record.upsertRecord

---
+++ src/main/java/com/cybozu/kintone/client/model/record/RecordsUpsertItem.java
@@ -0,0 +1,60 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2018 Cybozu
+ * https://github.com/kintone/kintone-java-sdk/blob/master/LICENSE
+ */
+
+package com.cybozu.kintone.client.model.record;
+
+import java.util.HashMap;
+
+import com.cybozu.kintone.client.model.record.field.FieldValue;
+
+public class RecordsUpsertItem {
+
+	private RecordUpdateKey updateKey;
+	private HashMap<String, FieldValue> record;
+
+	/**
+	 * Constructor
+	 * @param updateKey updateKey of the RecordUpsertItem
+	 * @param record record of the RecordUpsertItem
+	 */
+	public RecordsUpsertItem(RecordUpdateKey updateKey, HashMap<String, FieldValue> record) {
+		this.updateKey = updateKey;
+		this.record = record;
+    }
+    
+    /**
+	 * Get updateKey
+	 * @return updateKey
+	 */
+	public RecordUpdateKey getUpdateKey() {
+		return this.updateKey;
+    }
+    
+    /**
+	 * Set updateKey
+	 * @param updateKey the updateKey to set
+	 */
+    public void setUpdateKey(RecordUpdateKey updateKey) {
+        this.updateKey = updateKey;
+    }
+
+    /**
+	 * Get record
+	 * @return record
+	 */
+	public HashMap<String, FieldValue> getRecord() {
+		return this.record;
+    }
+    
+    /**
+	 * Set record
+	 * @param record the updateKey to set
+	 */
+    public void setRecord(HashMap<String, FieldValue> record) {
+        this.record = record;
+    }
+}
