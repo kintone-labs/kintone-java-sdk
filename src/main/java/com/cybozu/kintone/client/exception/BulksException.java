22671e68aff727ca1e7d2c61eed3364f1d646f3f
Ho Kim Cuc <c001106@PC-001106-4.local>
2019-07-04 15:22:14 +0700

    format code

--- src/main/java/com/cybozu/kintone/client/exception/BulksException.java
+++ src/main/java/com/cybozu/kintone/client/exception/BulksException.java
@@ -1,22 +1,20 @@
 package com.cybozu.kintone.client.exception;
 
+import java.util.ArrayList;
+
 /**
  * Exception will occur when using multiple bulk request.
  *
  */
 public class BulksException extends Exception{
 	private static final long serialVersionUID = 1L;
-	private Object results;
+	private ArrayList<Object> results;
 	public Object getResults() {
 		return results;
 	}
 
-	public BulksException(Object object) {
+	public BulksException(ArrayList<Object> object) {
 		this.results = object;
-	}
-	
-	public Object getResponse() {
-		return results;
 	}
 	
 }
