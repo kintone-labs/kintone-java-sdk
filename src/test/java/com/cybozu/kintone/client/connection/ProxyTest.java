7513edc41a3c05ce8ae393259c25c840ad67596b
Hoang Yen Minh <m001567@PC-001567-1.local>
2019-07-08 11:53:16 +0700

    unit test clear cache

--- src/test/java/com/cybozu/kintone/client/connection/ProxyTest.java
+++ src/test/java/com/cybozu/kintone/client/connection/ProxyTest.java
@@ -4,17 +4,35 @@ import com.cybozu.kintone.client.TestConstants;
 import com.cybozu.kintone.client.authentication.Auth;
 import com.cybozu.kintone.client.exception.KintoneAPIException;
 import com.cybozu.kintone.client.module.app.App;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 public class ProxyTest {
 
     private int APP_ID = 2;
     private Auth auth;
 
+    public static void clearAuthCache() {
+        try {
+            // this is evil, but there is no official way to clear the authentication cache...
+            // use of Reflection API so no sun classes are imported
+            Class<?> authCacheValueClass = Class.forName("sun.net.www.protocol.http.AuthCacheValue");
+            Class<?> authCacheClass = Class.forName("sun.net.www.protocol.http.AuthCache");
+            Class<?> authCacheImplClass = Class.forName("sun.net.www.protocol.http.AuthCacheImpl");
+            Constructor<?> authCacheImplConstructor = authCacheImplClass.getConstructor();
+            Method setAuthCacheMethod = authCacheValueClass.getMethod("setAuthCache", authCacheClass);
+            setAuthCacheMethod.invoke(null, authCacheImplConstructor.newInstance());
+        } catch (Throwable t) {
+        }
+    }
 
     @Before
     public void setup() {
+        clearAuthCache();
         Auth auth = new Auth();
         auth.setPasswordAuth(TestConstants.USERNAME, TestConstants.PASSWORD);
         this.auth = auth;
@@ -22,14 +40,11 @@ public class ProxyTest {
 
     @Test(expected = KintoneAPIException.class)
     public void checkAppWithWrongAccount() throws KintoneAPIException {
-        Auth auth2 = new Auth();
-        auth2.setPasswordAuth(TestConstants.USERNAME, TestConstants.PASSWORD);
-        Connection connection2 = new Connection(TestConstants.DOMAIN, auth2);
+        Connection connection2 = new Connection(TestConstants.DOMAIN, auth);
         connection2.setProxy(TestConstants.PROXY_HOST, TestConstants.PROXY_PORT, "hello", "bye");
 
         App app2 = new App(connection2);
         app2.getApp(APP_ID).getAppId();
-
     }
 
 
@@ -49,7 +64,7 @@ public class ProxyTest {
         connection.setProxy(TestConstants.PROXY_HOST, TestConstants.PROXY_PORT);
         App app = new App(connection);
         app.getApp(APP_ID).getAppId();
-       }
+    }
 
     @Test(expected = KintoneAPIException.class)
     public void checkAppWithHttpServer() throws KintoneAPIException {
@@ -60,13 +75,13 @@ public class ProxyTest {
         app.getApp(APP_ID).getAppId();
     }
 
-//    @Test
-//    public void checkAppWithRightAccount() throws KintoneAPIException {
-//        Connection connection = new Connection(TestConstants.DOMAIN, auth);
-//        connection.setProxy(TestConstants.PROXY_HOST, TestConstants.PROXY_PORT, "qasd", "Cybozu@123");
-//        App app = new App(connection);
-//
-//        Assert.assertNotNull(app.getApp(APP_ID).getAppId());
-//    }
+    @Test
+    public void checkAppWithRightAccount() throws KintoneAPIException {
+        Connection connection = new Connection(TestConstants.DOMAIN, auth);
+        connection.setProxy(TestConstants.PROXY_HOST, TestConstants.PROXY_PORT, TestConstants.PROXY_USERNAME, TestConstants.PROXY_PASSWORD);
+        App app = new App(connection);
+
+        Assert.assertNotNull(app.getApp(APP_ID).getAppId());
+    }
 
 }
