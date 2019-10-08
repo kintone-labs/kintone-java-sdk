5b2b5a4b99aa9d8f6cae4989196cc06bc2b26e95
minhhy2801 <minhhy2897@gmail.com>
2019-10-08 08:26:00 +0700

    implement BulksException toString, change function getResults to getResponses

--- src/main/java/com/cybozu/kintone/client/exception/BulksException.java
+++ src/main/java/com/cybozu/kintone/client/exception/BulksException.java
@@ -7,14 +7,29 @@ import java.util.ArrayList;
  */
 public class BulksException extends Exception {
     private static final long serialVersionUID = 1L;
-    private ArrayList<Object> results;
+    private ArrayList<Object> responses;
 
     public BulksException(ArrayList<Object> object) {
-        this.results = object;
+        this.responses = object;
     }
 
-    public ArrayList<Object> getResults() {
-        return results;
+    public ArrayList<Object> getResponses() {
+        return responses;
     }
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Integer failedBulkRequestIndex = 1;
+        for (Object bulkResponse : responses) {
+            if (bulkResponse.getClass().getSimpleName().equals("KintoneAPIException")) {
+                KintoneAPIException bulkError = (KintoneAPIException) bulkResponse;
+                sb.append("BulkRequest index: " + failedBulkRequestIndex.toString());
+                sb.append(", " + bulkError.toString());
+            } else {
+                failedBulkRequestIndex++;
+            }
+        }
+        return sb.toString();
+    }
 }
