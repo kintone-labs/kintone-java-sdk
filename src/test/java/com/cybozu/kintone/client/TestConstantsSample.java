fe2eed79e138ddaa0b3fad60145ab0ae9f03d4c8
anh-trinh <anh-trinh@cybozu.vn>
2019-10-23 10:01:29 +0700

    add TestConstantsSample

---
+++ src/test/java/com/cybozu/kintone/client/TestConstantsSample.java
@@ -0,0 +1,40 @@
+package com.cybozu.kintone.client;
+
+public class TestConstants {
+    public static final String DOMAIN = "https://xxx.cybozu.com";
+    public static final String HADOMAIN = "https://xxx.cybozu-dev.com";
+    public static final String BASIC_DOMAIN = "https://xxx.cybozu-dev.com";
+    public static final String WRONG_DOMAIN = "https://xxx.cybozu";
+    public static final String SECURE_DOMAIN = "https://xxx.s.cybozu.com";
+    public static final String HASECURE_DOMAIN = "https://xxx.s.cybozu-dev.com";
+    public static final String ADMIN_USERNAME = "xxx";
+    public static final String ADMIN_PASSWORD = "xxx";
+    public static final String BASIC_USERNAME = "xxxx";
+    public static final String BASIC_PASSWORD = "xxxx";
+    public static final String USERNAME = "xxx";
+    public static final String PASSWORD = "xxx";
+    public static int SPACE_ID;
+    public static int SPACE_APP_ID;
+    public static int SPACE_THREAD_ID;
+    public static int GUEST_SPACE_ID;
+    public static int GUEST_SPACE_APP_ID;
+    public static int GUEST_SPACE_THREAD_ID;
+    public static int MAX_VALUE;
+    public static final String PROXY_HOST = "xxx";
+    public static final String PROXY_HOST_HTTPS = "xxx";
+    public static int PROXY_PORT_HTTPS;
+    public static int PROXY_PORT;
+    public static String API_TOKEN = "xxxxxxxxxxxxxxxxxxxx";
+    public static String HAAPI_TOKEN = "xxxxxxxxxxxxxxxxxxxxxxx";
+    public static final String PROXY_USERNAME = "xxx";
+    public static final String PROXY_PASSWORD = "xxx";
+    public static String CLIENT_CERT_PATH = "src/test/resources/certificates/testUser/xxx.pfx";
+    public static String HACLIENT_CERT_PATH = "src/test/resources/certificates/testUser/xxx.pfx";
+    public static String WRONG_CLIENT_CERT_PATH = "src/test/resources/certificates/testUser/xxx.pfx";
+    public static String EXPIRED_CLIENT_CERT_PATH = "src/test/resources/certificates/testUser/xxx.pfx";
+    public static String UPLOAD_PATH = "src/test/resources/file/Upload/";
+    public static String DOWNLOAD_PATH = "src/test/resources/file/Download/";
+    public static String CLIENT_CERT_PASSWORD = "xxx";
+    public static String HACLIENT_CERT_PASSWORD = "xxx";
+    public static String EXPIRED_CLIENT_CERT_PASSWORD = "xxx";
+}
